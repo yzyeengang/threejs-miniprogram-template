455c2909ee1eb45da4e55433adbcb4b79b007bf6
yohane <yzyeengang@foxmail.com>
2025-05-01 16:32:43 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig, Rollup, normalizePath, Plugin, createFilter } from "vite";
 import uni from '@dcloudio/vite-plugin-uni';
 import threePlatformAdapter from '@minisheep/three-platform-adapter/plugin';
-import { toCustomChunkPlugin } from '@minisheep/rollup-plugin-to-custom-chunk';
 import { templateCompilerOptions } from '@tresjs/core'
 import { useStandardVuePlugin } from "@minisheep/platform-adapter-integration/tresjs/plugin";
 import glsl from 'vite-plugin-glsl';
 import { visualizer } from 'rollup-plugin-visualizer';
 import vue from '@vitejs/plugin-vue';
 import { babel } from '@rollup/plugin-babel';
+import createMpChunkSplitterPlugin from "@minisheep/vite-plugin-mp-chunk-splitter";
 
 const tresjsComponentPattern = /\/tresjs-demo\/examples\/.*.vue/
 
@@ -49,42 +49,17 @@ export default defineConfig({
       ...templateCompilerOptions
     }),
 
-    threePlatformAdapter({
-      mergePrefixGlobalOptions: {
-        include: [
-          /\/@vueuse\/(core|shared)\//,
-          /\/@tresjs\/(core|cientos)\//,
-        ]
-      },
-    }),
-
-
-    //默认情况 uni-app 会将node_modules下的模块全都打包在 common/vendor.js
-    //你也可以自定义输出目录，但需自己把控不影响应用加载顺序
-    //可以设置环境变量进行调试 如 cross-env DEBUG=minisheep:to-custom-chunk uni build -p mp-weixin
-    toCustomChunkPlugin({
-      'common/vendor':[
-
-      ],
-      'common/my-vendor': [
-        '@minisheep/three-platform-adapter',
-        '@minisheep/three-platform-adapter/wechat',
-      ],
-      'sub-pack-2/vendor': [
-        '@minisheep/three-platform-adapter/dist/three-override/jsm/**',
-        'three',
-        '@tresjs/core',
-        '@tresjs/cientos',
-        'three-stdlib',
-        'three/examples/jsm/**',
-        '@minisheep/platform-adapter-integration/tresjs'
-      ]
-    }),
-
     useStandardVuePlugin({
       include: [
         tresjsComponentPattern
       ]
-    })
+    }),
+    threePlatformAdapter(),
+    createMpChunkSplitterPlugin({
+      subpackages:['sub-pack-2'],
+      singleChunkMode:true,
+      packageSizeLimit:1.8*1024*1024
+    }),
+
   ],
 });
