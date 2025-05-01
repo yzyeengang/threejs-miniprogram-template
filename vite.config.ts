9d19c0953823cf298dd74e8c567a7a8d6712148b
yohane <yzyeengang@foxmail.com>
2025-05-01 15:22:16 +0800

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'weapp-vite/config';
 import threePlatformAdapter from '@minisheep/three-platform-adapter/plugin';
-import { toCustomChunkPlugin } from '@minisheep/rollup-plugin-to-custom-chunk';
 import glsl from 'vite-plugin-glsl';
 import { visualizer } from 'rollup-plugin-visualizer';
 import * as path from "node:path";
+import createMpChunkSplitterPlugin from "@minisheep/vite-plugin-mp-chunk-splitter";
 
 export default defineConfig({
   weapp: {
@@ -49,33 +49,34 @@ export default defineConfig({
   },
   plugins: [
     glsl(),
+    // @ts-ignore
     visualizer({
       emitFile: true,
     }),
     // @ts-ignore
     threePlatformAdapter({
       //插件默认会自动在入口 chunk 添加虚拟模块, 但此项目每个 page 都是入口会导致重复添加
-      mergePrefixGlobalOptions: {
+      prefixGlobal: {
         manualInject(chunk: any) {
           return chunk.name === 'src/app.ts';
         }
       }
     }),
-
-
-    //默认情况 weapp-vite 会将 package.json里的 dependencies 就近打包到 miniprogram_npm 见：https://vite.icebreaker.top/guide/npm.html
-    //你也通过将依赖全安装到 devDependencies ，然后通过自定义输出目录，更方便的使用分包
-    //可以设置环境变量进行调试 如 cross-env DEBUG=minisheep:to-custom-chunk uni build -p mp-weixin
-    toCustomChunkPlugin({
-      'common/my-vendor': [
-        '@minisheep/three-platform-adapter',
-        '@minisheep/three-platform-adapter/wechat',
-      ],
-      'sub-pack-2/vendor': [
-        '@minisheep/three-platform-adapter/dist/three-override/jsm/**',
-        'three',
-        'three/examples/jsm/**',
-      ]
-    })
+    // {
+    //   name:'123213',
+    //   enforce: 'post',
+    //   outputOptions(opts){
+    //     debugger;
+    //   },
+    //   generateBundle(o,bundle){
+    //     debugger;
+    //     console.log(bundle);
+    //   }
+    // }
+    createMpChunkSplitterPlugin({
+      subpackages:['sub-pack-2'],
+      singleChunkMode:true,
+      packageSizeLimit:1.8 * 1024*1024
+    }),
   ],
 })
