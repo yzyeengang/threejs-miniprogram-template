f4d4c5dfc6820624cfe5985b49dd50e9ace8d736
yohane <yzyeengang@foxmail.com>
2025-05-01 14:27:57 +0800

    update dependencies

--- src/game.ts
+++ src/game.ts
@@ -1,12 +1,9 @@
 import '@minisheep/mini-program-polyfill-core/wechat-polyfill';
 // import '@minisheep/mini-program-polyfill-core/xml-addon'; // 如果项目中使用了需要 DOMParser 支持的部分，也可以后续按需导入，避免影响主包大小
+import '@minisheep/three-platform-adapter/wechat-game';
 import { game } from '@minisheep/three-platform-adapter';
-import { wechatGame } from '@minisheep/three-platform-adapter/wechat-game';
 import {init as gltfDemo} from "@/demos/gltf-loader";
 import {init as particles} from "@/demos/particles/index";
-game.useAdapter(wechatGame);
-
-game.patch('THREEGlobals');
 
 
 const result = game.useCanvas();
